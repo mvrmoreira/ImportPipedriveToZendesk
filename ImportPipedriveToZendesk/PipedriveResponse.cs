75da74f4bff3a754105bdb45de99209856b695b5
Matheus Moreira <mvrmoreira@gmail.com>
2014-07-23 19:21:10 -0300

    first commit

---
+++ ImportPipedriveToZendesk/PipedriveResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportPipedriveToZendesk {
+    class PipedriveResponse {
+        public bool success { get; set; }
+        public List<PipedrivePerson> data { get; set; }
+        public PipedriveAdditionalData additional_data { get; set; }
+    }
+
+    class PipedrivePerson {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    class PipedriveAdditionalData {
+        public PipedrivePagination pagination { get; set; }
+    }
+
+    class PipedrivePagination {
+        public int start { get; set; }
+        public int limit { get; set; }
+        public bool more_items_in_collection { get; set; }
+        public int next_start { get; set; }
+    }
+}
---
+++ ImportPipedriveToZendesk/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using ZendeskApi_v2;
+
+//https://github.com/eneifert/ZendeskApi_v2
+//https://developers.pipedrive.com/v1
+namespace ImportPipedriveToZendesk {
+    class Program {
+        static void Main(string[] args) {
+
+            GetPipedrivePersonByEmail("teste");
+
+            ZendeskApi api = new ZendeskApi("https://mundipagg.zendesk.com/api/v2", "xxxxxxxxxx@xxxxxxxxxxx.com", "", "xxxxxxxxxxxxxxxxxxx");
+            var usersResponse = api.Users.GetAllUsers();
+            foreach (var user in usersResponse.Users) {
+                
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static PipedriveResponse GetPipedrivePersonByEmail(string email) {            
+
+            string sURL = "http://api.pipedrive.com/v1/persons/find?term=email@email.com&start=0&search_by_email=1&api_token=xxxxxxxxxxxxxxxxxxx";
+            WebRequest wrGETURL = WebRequest.Create(sURL);
+            Stream objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
+            string sLine = objReader.ReadLine();
+            PipedriveResponse pipedriveResponse = JsonConvert.DeserializeObject<PipedriveResponse>(sLine);
+
+            return pipedriveResponse;
+        }
+    }
+}
